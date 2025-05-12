b186d39a9d2c6c6e9bc40cc5d722aa823e9d20bd
monicag-gupta <86832090+monicag-gupta@users.noreply.github.com>
2025-05-12 16:45:00 +0530

    Update NumpyEg1.py

--- NumpyEg1.py
+++ NumpyEg1.py
@@ -38,3 +38,29 @@ print("Reshaped Array:\n", arr_reshaped)
 # [[1 2 3]
 # [4 5 6]]
 
+
+
+
+
+#numpy3
+
+# NumPy supports multiple data types (e.g., int, float, complex), and you can specify the data type when creating arrays.
+# Creating an array with a specific data type
+import numpy as np
+arr = np.array([1, 2, 3])  #Checking the Data Type
+print("Array: ", arr, "Data Type: ", arr.dtype) # Output: int64 (on most 64-bit systems)
+arr2 = np.array([1, 'hello', True], dtype=object)
+print("Array: ", arr2, "Data Type: ", arr2.dtype)  # object
+
+arr_float = np.array([1, 2, 3, 4], dtype=np.float32)
+print("Array with Float DataType:", arr_float)
+
+# Create a complex number array with a specific data type
+complex_arr_dtype = np.array([1 + 2j, 3 + 4j, 5 + 6j], dtype=np.complex64)
+print("\nComplex Number Array with dtype:", complex_arr_dtype)
+
+# Create a complex number array
+complex_arr = np.array([1 + 2j, 3 + 4j, 5 + 6j])
+print("\nComplex Number Array:", complex_arr)
+print("Data type of the array:", complex_arr.dtype)
+
