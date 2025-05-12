d65bc756fe23ab3c5c88ed59c083d5948c8a48f0
monicag-gupta <86832090+monicag-gupta@users.noreply.github.com>
2025-05-12 15:28:40 +0530

    Update ForEg.py

--- ForEg.py
+++ ForEg.py
@@ -1,11 +1,19 @@
 # Iterating through a list
+print("\nFruits:")
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print(fruit)
-
+print("\nFor : range(1,6): ")
 # Using range() to generate a sequence of numbers
 for i in range(1, 6):
     print(i)
-
+print("\nFor : range(6,0,-1): ")
 for i in range(6,0,-1):
     print(i)
+
+print("\nWhile count=1 to 5: ")
+# Counting using a while loop
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
