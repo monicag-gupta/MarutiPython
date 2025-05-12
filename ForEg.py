26396e06adea771119a6e8df33fcede065c7c2ff
monicag-gupta <86832090+monicag-gupta@users.noreply.github.com>
2025-05-12 15:20:16 +0530

    Create ForEg.py

---
+++ ForEg.py
@@ -0,0 +1,8 @@
+# Iterating through a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Using range() to generate a sequence of numbers
+for i in range(1, 6):
+    print(i)
