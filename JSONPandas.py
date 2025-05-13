03099d7afd2f6fae044965d88d0d15339c9c81b3
monicag-gupta <86832090+monicag-gupta@users.noreply.github.com>
2025-05-13 14:50:00 +0530

    Update JSONPandas.py

--- JSONPandas.py
+++ JSONPandas.py
@@ -18,3 +18,30 @@ with open(file_name, 'w') as json_file:
 
 print(f"Dictionary has been written to {file_name}")
 
+
+
+#####################
+
+import json
+
+# Specify the file name
+file_name = "data.json"
+
+# Read the JSON file
+with open(file_name, 'r') as json_file:
+    data = json.load(json_file)
+
+# Print the dictionary
+print("Data read from JSON file:")
+print(data)
+
+# Pretty print the JSON data
+print("\nData as JSON printed in pretty format:")
+print(json.dumps(data, indent=4))
+
+
+
+
+
+
+
