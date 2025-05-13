91147d48be386d2b9f94646c416e89b803d0d81e
monicag-gupta <86832090+monicag-gupta@users.noreply.github.com>
2025-05-13 14:34:33 +0530

    Create CSVPandas.py

---
+++ CSVPandas.py
@@ -0,0 +1,8 @@
+employees.csv
+
+Name,Age,Department,Salary,Joining Date
+Alice,25,HR,50000,2022-01-01
+Bob,30,IT,60000,2021-05-15
+Charlie,35,Finance,70000,2020-03-10
+David,40,IT,80000,2019-07-30
+Eve,28,HR,55000,2023-02-15
