aff950705446083c34f03b3424481024daf52441
monicag-gupta <86832090+monicag-gupta@users.noreply.github.com>
2025-05-13 15:09:41 +0530

    Update PandasEg3.py

--- PandasEg3.py
+++ PandasEg3.py
@@ -101,3 +101,80 @@ print("\nOuter Joined DataFrame (Employee info with salary):")
 print(joined_df)
 
 
+
+
+
+
+###########
+
+
+import pandas as pd
+
+# Sample DataFrame
+data = {
+    'Employee_ID': [101, 102, 103, 104, 105, 106, 107],
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'John'],
+    'Department': ['HR', 'Finance', 'IT', 'HR', 'HR', 'IT', 'Finance'],
+    'Salary': [60000, 70000, 80000, 65000, 75000, 85000, 89000],
+    'Age': [25, 30, 35, 40, 25, 35, 50]
+}
+
+df = pd.DataFrame(data)
+
+# Group by 'Department' and apply aggregation functions
+
+print("\nEmployee count by department:")
+print(df.groupby("Department")["Employee_ID"].count())
+
+print("Group by Department - Salary Sum:")
+print(df.groupby('Department')['Salary'].sum())
+
+print("\nGroup by Department - Salary Mean:")
+print(df.groupby('Department')['Salary'].mean())
+
+print("\nGroup by Department - Salary Median:")
+print(df.groupby('Department')['Salary'].median())
+
+print("\nGroup by Department - Salary Min:")
+print(df.groupby('Department')['Salary'].min())
+
+print("\nGroup by Department - Salary Max:")
+print(df.groupby('Department')['Salary'].max())
+
+print("\nGroup by Department - Salary Standard Deviation:")
+print(df.groupby('Department')['Salary'].std())
+
+print("\nGroup by Department - Salary Standard Variance:")
+print(df.groupby('Department')['Salary'].var())
+
+print("\nGroup by Department - First salary in group:")
+print(df.groupby('Department')['Salary'].first())
+
+print("\nGroup by Department - Last salary in group:")
+print(df.groupby('Department')['Salary'].last())
+
+print("\nGroup by Department - Custom Aggregation (Min and Max):")
+print(df.groupby('Department').agg({
+    'Salary': ['min', 'max'],
+    'Age': 'mean'
+}))
+
+print("\nGroup by Department - Custom Function(max-min) to group:")
+print(df.groupby('Department')['Salary'].apply(lambda x: x.max() - x.min()))
+
+print("\nGroup by Department - transform to a new dataframe with same size as df:")
+print(df.groupby('Department')['Salary'].transform(lambda x: x - x.mean()))
+
+print("\nGroup by Department - size of each row:")
+print(df.groupby('Department')['Salary'].size())
+
+print("\nGroup by Department - Cross-tabulation of two or more variables:")
+print(pd.crosstab(df['Department'], df['Age']))
+
+print("\nGroup by Department - statistics for each group (count, mean, std, min, 25%, 50%, 75%, and max:")
+print(df.groupby('Department').describe())
+
+print("\nGroup by Department - Filter Groups with Mean Salary > 70000:")
+print(df.groupby('Department').filter(lambda x: x['Salary'].mean() > 70000))
+
+
